18bf16bca463567faf2aada941eb1b444ff6a2e9
ieraasyl <95128018+ieraasyl@users.noreply.github.com>
2025-09-13 16:05:29 +0500

    a

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_API_BASE;
+
 export const analyzeCarImage = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
